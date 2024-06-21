eacab6e033c4d1050d39f2c061b0047027c7fcf1
Javier Borquez <163423384+JavierBor@users.noreply.github.com>
2024-06-21 01:46:52 +0000

    implementación menú

--- .ccls-cache/@home@runner@Proyecto-estructura/main.c
+++ .ccls-cache/@home@runner@Proyecto-estructura/main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "TDAs/extra.h"
+#include "TDAs/list.h"
+#include "TDAs/hashmap.h"
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+
+void imprimirMenu(){
+  printf("1) Cargar pokémon.\n");
+  printf("2) Buscar pokémon por nombre.\n");
+  printf("3) Buscar pokémon por número.\n");
+  printf("4) Buscar pokémon por tipo.\n");
+  printf("5) Buscar pokémon por generacion.\n");
+  printf("6) Salir.\n");
+  return;
+}
+
+
+int main(void){
+  int opcion;
+
+  do{
+    imprimirMenu();
+    printf("Ingrese una opcion: ");
+    scanf("%i", &opcion);
+    switch(opcion){
+      case 1:
+        printf("Cargando pokémon\n");
+        break;
+      case 2:
+        printf("Buscando pokémon por nombre\n");
+        break;
+      case 3:
+        printf("Buscando pokémon por número\n");
+        break;
+      case 4:
+        printf("Buscando pokémon por tipo\n");
+        break;
+      case 5:
+        printf("Buscando pokémon por generacion\n");
+        break;
+      case 6:
+        printf("Saliendo\n");
+        break;
+      default:
+        printf("Opción no válida\n");
+        break;
+    }
+    presioneTeclaParaContinuar();
+    limpiarPantalla();
+  }while (opcion != 6);
 }
